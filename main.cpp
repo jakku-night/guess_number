35e9ae6338a866066057b98b94db12d985e26a63
Jakku Night <J97k107u45k117n@gmail.com>
2021-10-16 04:48:03 +0000

    Final Project with reset and input validation.

--- main.cpp
+++ main.cpp
@@ -1,95 +1,153 @@
+// Program Name: Guess Number
+// Includes:
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
+// Global Variables:
 bool end_game = false;
 bool err = false;
-int rand_number[4] = {0, 0, 0, 0};
-int guess_number[4] = {0, 0, 0, 0};
-char input_number[5] = {0, 0, 0, 0, 0};
+int a = 0, b = 0;
+int number[4] = {0, 0, 0, 0};
+int input_number[4] = {0, 0, 0, 0};
+char input[5] = {0, 0, 0, 0, 0};
 
-bool enter_number(){
-    cin >> input_number;
-    bool error = true;
-    for(int i = 0; i < 4; i++){
-        if((int)input_number[i] < 48 || (int)input_number[i] > 58){
-            // Hacer saltar error
-            error = false;
-        }else{
-            guess_number[i] = (int)input_number[i] -48;
-        }
-        // Resta checkear los numeros repetidos:
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                if(i != j && guess_number[i] == guess_number[j]){
-                    error = false;                    
-                }
-            }
-        }
-    }
-    return error;
+// Global Declarations:
+void gen_number();
+void enter_number();
+bool compare_numbers();
+void print_vector(int* vector);
+void guess_loop();
+void display_instructions();
+void prompt_reset();
+
+// Main:
+int main(){
+
+    do{
+        gen_number();
+        display_instructions();
+        guess_loop();
+    }while(!end_game);
+    prompt_reset();
+    return 0;
 }
 
-void generate_number(){
+// Global Definitions:
+
+void display_instructions(){
+    cout << "Try to guess the number!" << endl;
+    cout << "Try to guess the number by input a 4-not-repeated digit number." << endl;
+    cout << "The program will give you some hints to reach the goal." << endl;
+    cout << "The letter 'A' represents the digits you guessed in the correct position." << endl;
+    cout << "The letter 'B' represents the digits you guessed in the wrong position." << endl;
+}
+
+void gen_number(){
+    // Charges the array with random numbers:
     for(int i = 0; i < 4; i++){
-        rand_number[i] = rand() % 10;
+        number[i] = rand() % 10;
     }
+    // Checks each element to be diffrent from each other and changes the value if equals.
     for(int i = 0; i < 4; i++){
         for(int j = 0; j < 4; j++){
-            while(rand_number[i] == rand_number[j] && i != j){
-                rand_number[i] = rand() % 10;
-                // n / 10 => R = 0 - 9
+            if(i != j){
+                while(number[i] == number[j]){
+                    number[i] = rand() % 10;
+                }
             }
-        }
+        }        
+    }
+    
+}
+
+void print_vector(int* vector, int length){
+    for(int i = 0; i < length; i++){
+        cout << vector[i] << endl;
     }
 }
 
-void compare_numbers(){
-    if(!err){
-        int a = 0;
-        int b = 0;
+void enter_number(){
+    cout << "Insert your number:" << endl;
+    do{
+        err = false;
+        cin >> input;
         for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                if(rand_number[j] == guess_number[i]){
-                    if(j == i){
-                        a++;
-                    }else{
-                        b++;
+            if((int)input[i] < 48 || (int)input[i] > 58){
+                err = true;
+            }
+        }
+        for(int i = 0; i < 4; i++){
+            for(int j= 0; j < 4; j++){
+                if(i != j){
+                    if((int)input[i] == input[j]){
+                        err = true;
                     }
                 }
             }
         }
-        printf("%iA%iB\n", a, b);
-        if(a == 4){
-            cout << "You Win!" << endl;
-            cout << "Will you try again? [Y|N]: " << endl;
-            char res;
-            cin >> res;
-            if(res == 'Y' || res == 'y'){
-                end_game = false;
+        if(err){
+            cout << "Bad input. Try again." << endl;
+        }else{
+            for (int i = 0; i < 4; i++){
+                input_number[i] = (int)input[i] - 48;
             }
-            if(res == 'N' || res == 'n'){
-                end_game = true;
-            }
-            err = true;
         }
+    }while(err);
+}
+
+bool compare_numbers(){
+    for(int i = 0; i < 4; i++){
+        for(int j= 0; j < 4; j++){
+            if(input_number[i] == number[j]){
+                if(i == j){
+                    a++;
+                }else{
+                    b++;
+                }
+            }
+        }
+    }
+    cout << "Digits in place: " << a << "A" << endl;
+    cout << "Digits in the number: " << b << "B" << endl;
+    if(a < 4){
+        return false;
+    }else{
+        return true;
     }
 }
 
-int main(){
+void guess_loop(){
+    bool end_loop = false;
+    do{
+        a = 0;
+        b = 0;
+        enter_number();
+        end_loop = compare_numbers();
+    }while(!end_loop);
+}
 
-    cout << "Try to guess the number!" << endl;
-    while(!end_game){
-        bool entry = false;
-        generate_number();
-        err = false;
-        while(!err){
-            while(!entry){
-                entry = enter_number();
-            }
-            compare_numbers();
-        }
+void prompt_reset(){
+    char res = '0';
+    cout << "You Win!!!" << endl;
+    cout << "Would you want to try again? [Y/n]:" << endl;
+    cin >> res;
+    switch (res){
+        case 'Y':
+            end_game = false;
+            break;
+        case 'y':
+            end_game = false;
+            break;
+        case 'N':
+            end_game = true;
+            break;
+        case 'n':
+            end_game = true;
+            break;
+        default:
+            end_game = false;
+            break;
     }
-
-    return 0;
 }
